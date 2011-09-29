272324d2b2484521b2297a5495d98878327a1bb9
jimwebber <jim@jimwebber.org>
2011-09-29 22:35:56 +0100

    Tidying up imports.

--- src/main/java/org/neo4j/server/extension/auth/AuthenticationFilter.java
+++ src/main/java/org/neo4j/server/extension/auth/AuthenticationFilter.java
@@ -19,12 +19,18 @@
  */
 package org.neo4j.server.extension.auth;
 
-import sun.misc.BASE64Decoder;
+import java.io.IOException;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import sun.misc.BASE64Decoder;
 
 /**
  * @author tbaum
